0898a7e2be6174d51603bcfb986c9e0a9ff64f95
Fernandez-17 <crfernandezc@unjbg.edu.pe>
2024-06-18 01:31:52 -0500

    Se agrego ejercicio de suma de impares de estructuras repetitivas

---
+++ SUMA DE IMPARES.cpp
@@ -0,0 +1,16 @@
+/*Diseñar un algoritmo que sume todos los números impares hasta un número "n" dado.*/
+
+#include<iostream>
+using namespace std;
+int main( ){
+	int numero,suma_de_impares=0;
+	cout<<"Ingrese un numero: ";
+	cin>>numero;
+	for(int i=1;i<=numero;i++){
+		if(i%2!=0){	
+		suma_de_impares=suma_de_impares+i;
+		}
+	}
+	cout<<"La suma total de numeros impares es de: "<<suma_de_impares<<endl;
+	return 0;
+}
