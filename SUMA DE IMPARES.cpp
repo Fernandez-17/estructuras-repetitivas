3520ea99f86b7cad3885536edfdd4c5fc6375b55
Fernandez-17 <crfernandezc@unjbg.edu.pe>
2024-06-18 23:58:06 -0500

    Se actualizo el ejercicio de suma de impares: se agrego condicional para invalidar numeros negativos

--- SUMA DE IMPARES.cpp
+++ SUMA DE IMPARES.cpp
@@ -6,11 +6,17 @@ int main( ){
 	int numero,suma_de_impares=0;
 	cout<<"Ingrese un numero: ";
 	cin>>numero;
-	for(int i=1;i<=numero;i++){
+	if (numero<0){
+		cout<<"El numero ingresado no es valido";
+	}else {
+		for(int i=1;i<=numero;i++){
 		if(i%2!=0){	
-		suma_de_impares=suma_de_impares+i;
+		suma_de_impares=suma_de_impares+i;	
+		
 		}
 	}
-	cout<<"La suma total de numeros impares es de: "<<suma_de_impares<<endl;
-	return 0;
+	cout<<"La suma total de numeros impares es de: "<<suma_de_impares<<endl;	
 }
+    return 0;
+}
+
