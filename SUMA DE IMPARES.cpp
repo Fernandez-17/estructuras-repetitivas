233de8433be0532c3fc1456c0ee1e4b8196ccb2d
Fernandez-17 <crfernandezc@unjbg.edu.pe>
2024-06-19 00:32:41 -0500

    Se actualizo el ejercicio de SUMA DE IMPARES: se agrego una funcion para realizar especificamente la suma de numeros impares

--- SUMA DE IMPARES.cpp
+++ SUMA DE IMPARES.cpp
@@ -2,21 +2,26 @@
 
 #include<iostream>
 using namespace std;
+
+int suma_impar( int num){
+	int suma=0;
+	for(int i=1;i<=num ;i++){
+	if(i%2!=0){	
+	suma=suma+i;
+		}
+	}
+	return suma;
+}
+
 int main( ){
 	int numero,suma_de_impares=0;
 	cout<<"Ingrese un numero: ";
 	cin>>numero;
 	if (numero<0){
-		cout<<"El numero ingresado no es valido";
+		cout<<"El numero ingresado no es valido"<<endl;
 	}else {
-		for(int i=1;i<=numero;i++){
-		if(i%2!=0){	
-		suma_de_impares=suma_de_impares+i;	
-		
-		}
-	}
-	cout<<"La suma total de numeros impares es de: "<<suma_de_impares<<endl;	
-}
+	   suma_de_impares=suma_impar(numero);
+	   cout<<"La suma total de numeros impares es de: "<<suma_de_impares<<endl;	
+	   }
     return 0;
 }
-
